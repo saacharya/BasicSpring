b1ef0d5320d7c862f3f808e668e3fb0959c93f85
santosh <acharya.santosh@gmail.com>
2019-04-12 11:31:36 +0530

    first

--- complete/src/main/java/com/controller/GreetingController.java
+++ complete/src/main/java/com/controller/GreetingController.java
@@ -1,19 +1,20 @@
-package hello;
+package com.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.domain.Greeting;
+
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    @RequestMapping("/autoincrease")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="Santosh") String name) {
         return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+                            "Welcome "+ name);
     }
 }
---
+++ complete/src/test/java/com/GreetingControllerTests.java
@@ -0,0 +1,39 @@
+package com;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GreetingControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void noParamDefaultMessage() throws Exception {
+
+        this.mockMvc.perform(get("/autoincrease")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Welcome Santosh"));
+    }
+
+    @Test
+    public void paramGreetingCustomMessage() throws Exception {
+
+        this.mockMvc.perform(get("/autoincrease").param("name", "Spock"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Welcome Spock"));
+    }
+
+}
